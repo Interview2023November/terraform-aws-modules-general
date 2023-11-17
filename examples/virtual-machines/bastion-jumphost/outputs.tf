fc8927b799b6fb8ea5d94bf69fdd20f09d27ff20
forjor <jordan.interviews@gmail.com>
2023-11-17 08:32:06 -0400

    feat: Add test for bastion and shell for example (test is currently RED).

---
+++ examples/virtual-machines/bastion-jumphost/outputs.tf
@@ -0,0 +1,9 @@
+output "bastion_public_ip" {
+  value       = "fake"
+  description = "The public IP of the bastion."
+}
+
+output "backend_private_ip" {
+  value       = "fake"
+  description = "The public IP of the bastion."
+}
--- examples/virtual-machines/bastion-jumphost/variables.tf
+++ examples/virtual-machines/bastion-jumphost/variables.tf
@@ -3,24 +3,8 @@
 # You must provide a value for each of these parameters.
 # ---------------------------------------------------------------------------------------------------------------------
 
-variable "name" {
-  description = "The for the DB server."
-  type        = string
-}
-
-variable "db_username" {
-  description = "The username for the DB."
-  type        = string
-}
-
-variable "db_password" {
-  description = "The password for the DB."
-  type        = string
-  sensitive   = true
-}
-
-variable "db_name" {
-  description = "The name of the initial database."
+variable "key_pair_name" {
+  description = "The name of the keypair which will be used for SSH access to both servers."
   type        = string
 }
 
@@ -33,16 +17,4 @@ variable "region" {
   description = "The the region where the DB is located."
   type        = string
   default     = "us-east-2"
-}
-
-variable "instance_class" {
-  description = "The type of machine the DB will run on."
-  type        = string
-  default     = "db.t2.micro"
-}
-
-variable "port" {
-  description = "The port where the DB server will host connections."
-  type        = number
-  default     = 3306
 }
