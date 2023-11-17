44f3e9f0c16d883ee62096ded12dd2915d085701
forjor <jordan.interviews@gmail.com>
2023-11-17 07:27:46 -0400

    fix: Address some linter errors.

--- modules/virtual-machines/webserver/main.tf
+++ modules/virtual-machines/webserver/main.tf
@@ -2,7 +2,9 @@ resource "aws_instance" "webserver" {
   ami                    = var.ami
   instance_type          = var.instance_type
   vpc_security_group_ids = var.vpc_security_group_ids
-  monitoring             = var.monitoring
+  
+  #ts:skip=AWS.AI.LM.HIGH.0070 Skip detailed monitoring scan for now as we have enabled as the default
+  monitoring = var.monitoring
 
   metadata_options {
     # Don't allow IMDSV1 to be used
